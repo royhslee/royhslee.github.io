a9a90c00d1f05b628169d947aaa7690abf645ffa
royhslee <36047514+royhslee@users.noreply.github.com>
2018-03-29 18:54:06 -0700

    Draft
    html css javascript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('ready');
+$('#calendar').datepicker({
+	inline: true,
+	firstDay: 1,
+	showOtherMonths: true,
+	dayNamesMin: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	autoSize: true
+
+});
